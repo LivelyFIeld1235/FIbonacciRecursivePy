f4fd006c5cee0c053b40c167ec9ac447f8810b1a
LivelyFIeld1235 <luvg.rathod@gmail.com>
2022-03-25 20:55:31 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+def fibonacci_recursiveFUNC(nth_POS):
+    primitives = [1,2]
+    if nth_POS in primitives:
+        return nth_POS -1
+    else:
+        return fibonacci_recursiveFUNC(nth_POS-1) + fibonacci_recursiveFUNC(nth_POS -2)
+
+def main():
+    exitCONDITION = ['exit','quit', 'break']
+
+    nth_Pos = input("Enter a valid nth POS value")
+    while nth_Pos not in exitCONDITION:
+        try:
+            fibonacci_recursiveFUNC(int(nth_Pos))
+        except Exception:
+            print("something went wrong")
+        else:
+            nth_Pos = input("Enter a valid nth POS value")
+
+if __name__ == '__main__':
+    main()
